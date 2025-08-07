ff0b67accfbee6928b3b1ac8588f5178aef211d6
Derrick Timmermans <derrick.timmermans@outlook.com>
2025-08-07 14:31:10 +0200

    Initial template

---
+++ SampleEDMOPlugin.cs
@@ -0,0 +1,14 @@
+﻿using ServerVNext.EDMO;
+using ServerVNext.EDMO.Plugins;
+
+namespace EDMOPluginTemplate;
+
+public class SampleEDMOPlugin(EDMOSession session) : EDMOPlugin(session)
+{
+    public override string PluginName { get; } = nameof(SampleEDMOPlugin);
+
+    public override void FrequencyChangedByUser(int userIndex, float freq)
+    {
+        SetFrequency(1.3f);
+    }
+}
